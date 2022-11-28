a46ba21e1771a44fe9af31d7344d10a1b296f224
DroidFreak32 <rushabshah32@gmail.com>
2022-11-28 15:14:15 +0000

    lineage_sdk_phone_x86: Extend PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST
    Change-Id: I6372569c2ff62cfc1aaea40ffa19e36e9dc71dcc

--- build/target/product/lineage_sdk_phone_x86.mk
+++ build/target/product/lineage_sdk_phone_x86.mk
@@ -20,6 +20,17 @@ include vendor/lineage/build/target/product/lineage_generic_target.mk
 PRODUCT_ENFORCE_ARTIFACT_PATH_REQUIREMENTS := relaxed
 MODULE_BUILD_FROM_SOURCE := true
 
+PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
+  system/app/GoogleExtShared/GoogleExtShared.apk \
+  system/app/GooglePrintRecommendationService/GooglePrintRecommendationService.apk \
+  system/etc/permissions/game-overlay.xml \
+  system/etc/permissions/privapp-permissions-google.xml \
+  system/lib/libRSSupport.so \
+  system/lib/libblasV8.so \
+  system/lib/librsjni.so \
+  system/addon.d/70-velvet.sh \
+  system/app/FlipendoPrebuilt/FlipendoPrebuilt.apk
+
 # Overrides
 PRODUCT_NAME := lineage_sdk_phone_x86
 PRODUCT_MODEL := LineageOS Android SDK built for x86
