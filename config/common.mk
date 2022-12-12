Evolution-X-AOSP/vendor_evolution

vendor: Smart Charging (4/4)
Change-Id: I51966875aaacc4b0068772ae732abab16652c973

config/common.mk
@@ -132,7 +132,8 @@ PRODUCT_COPY_FILES += \
 
 # Copy all Evolution X-specific init rc files
 PRODUCT_COPY_FILES += \
-    vendor/evolution/prebuilt/common/etc/init/init.evolution-x-updater.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.evolution-x-updater.rc
+    vendor/evolution/prebuilt/common/etc/init/init.evolution-x-updater.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.evolution-x-updater.rc \
+    vendor/evolution/prebuilt/common/etc/init/smartcharge-init.rc:$(TARGET_COPY_OUT_SYSTEM)/etc/init/smartcharge-init.rc
 
 # Enable Android Beam on all targets
 PRODUCT_COPY_FILES += \

ROM-EXTRAS/android_vendor_lineage

config/common.mk
@@ -121,6 +121,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     vendor/lineage/prebuilt/common/etc/init/init.lineage-updater.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.lineage-updater.rc
 
+PRODUCT_COPY_FILES += \
+    vendor/lineage/prebuilt/common/etc/init/smartcharge-init.rc:$(TARGET_COPY_OUT_SYSTEM)/etc/init/smartcharge-init.rc
+
 # Themes
 PRODUCT_PACKAGES += \
     LineageBlackTheme \